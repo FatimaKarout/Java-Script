222216e19c7a6b39198ee9e56fb33241ee2a9afc
unknown <fatimakarout1717@gmil.com>
2023-07-27 10:35:46 +0300

    Done with task 7

---
+++ task-4/task-4.js
@@ -0,0 +1,5 @@
+let fatima= Array ();
+const countries = [];
+
+
+
---
+++ task-5/task-5.js
@@ -0,0 +1,8 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10] 
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      console.log(i + " is even");
+    } else {
+      console.log(i + " is odd");
+    }
+  }
